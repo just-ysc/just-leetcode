eebc9ef8f6961cd60a2d8ac0e4807253aded83e0
just-ysc <cks6589@snu.ac.kr>
2024-03-12 01:31:24 +0900

    Time: 6 ms (32.57%), Space: 44.5 MB (71.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,60 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
         int result = 0;
+        Map<Character, Integer> romanToIntMap = new HashMap<>();
+        romanToIntMap.put('I', 1);
+        romanToIntMap.put('V', 5);
+        romanToIntMap.put('X', 10);
+        romanToIntMap.put('L', 50);
+        romanToIntMap.put('C', 100);
+        romanToIntMap.put('D', 500);
+        romanToIntMap.put('M', 1000);
+
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == 'I') {
-                if (i < s.length() - 1) {
-                    if (s.charAt(i + 1) == 'V') {
-                        result += 4;
-                        i++;
-                    } else if (s.charAt(i + 1) == 'X') {
-                        result += 9;
-                        i++;
-                    } else {
-                        result++;
-                    }
-                } else {
-                    result++;
-                }
-            } else if (c == 'V') {
-                result += 5;
-            } else if (c == 'X') {
-                if (i < s.length() - 1) {
-                    if (s.charAt(i + 1) == 'L') {
-                        result += 40;
-                        i++;
-                    } else if (s.charAt(i + 1) == 'C') {
-                        result += 90;
-                        i++;
-                    } else {
-                        result += 10;
-                    }
-                } else {
-                    result += 10;
-                }
-            } else if (c == 'L') {
-                result += 50;
-            } else if (c == 'C') {
-                if (i < s.length() - 1) {
-                    if (s.charAt(i + 1) == 'D') {
-                        result += 400;
-                        i++;
-                    } else if (s.charAt(i + 1) == 'M') {
-                        result += 900;
-                        i++;
-                    } else {
-                        result += 100;
-                    }
-                } else {
-                    result += 100;
-                }
-            } else if (c == 'D') {
-                result += 500;
-            } else if (c == 'M') {
-                result += 1000;
+            int romanToInt = romanToIntMap.get(s.charAt(i));
+            if (i < s.length() - 1 && (romanToInt < romanToIntMap.get(s.charAt(i + 1)))) {
+                result -= romanToInt;
+            } else {
+                result += romanToInt;
             }
         }
+        
         return result;
     }
 }
