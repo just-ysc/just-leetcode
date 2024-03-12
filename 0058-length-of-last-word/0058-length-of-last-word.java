79e5dedcc2339e94c3c38afef30359ba2c23bcb4
just-ysc <cks6589@snu.ac.kr>
2024-03-12 22:49:36 +0900

    Time: 3 ms (5.08%), Space: 41.6 MB (65.54%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int counter = 0;
-        boolean newWord = false;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == ' ') {
-                newWord = true;
-            } else {
-                counter = newWord ? 1 : counter + 1;
-                newWord = false;
-            }
-        }
-        return counter;
+        return Arrays.stream(s.split(" "))
+        .map(word -> word.length())
+        .filter(wordLength -> wordLength > 0)
+        .reduce(0, (_, cur) -> cur);
     }
 }
