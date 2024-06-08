ef75bdba6f4d4a43b09f21807c3d5b91b9cb145a
just-ysc <cks6589@snu.ac.kr>
2024-06-09 00:04:41 +0900

    Time: 82 ms (27.72%), Space: 53.8 MB (10.42%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,14 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n <= 0) return false;
-    let cnt = 0;
-    let mask = 1;
-    while (n > 0) {
-        const masked = n & mask;
-        if (masked === 1) cnt++;
-        if (cnt > 1) return false;
-        n >>= 1;
-    }
-    return true;
+    return n > 0 && (n & (n - 1)) === 0;
 };
